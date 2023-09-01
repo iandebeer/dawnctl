7eeeaf4e2022ca63d0f53ca739170d2b4fbe683e
iandebeer <ian@mn8.dev>
2023-09-01 11:01:30 +0200

    take-on

---
+++ xyz/didx/DawnCtlSpec.test.scala
@@ -0,0 +1,11 @@
+//> using toolkit typelevel:latest
+
+package xyz.didx
+
+class DawnCtlSpec extends munit.FunSuite {
+   test("sum of two integers") {
+    val obtained = 2 + 2
+    val expected = 4
+    assertEquals(obtained, expected)
+  }
+}
